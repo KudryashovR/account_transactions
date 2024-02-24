2995adc6f7bff5e1ec2a2f999ed639378e4084be
Roman Kudryashov <romkud55@gmail.com>
2024-02-24 15:28:19 +0300

    ADD: Add print_operations func to main.py

--- account_transactions/main.py
+++ account_transactions/main.py
@@ -5,7 +5,9 @@ def main():
     filename = "operations.json"
 
     operations_json = utils.load_operations(filename)
-    operations_list = utils.init_operations(operations_json).sort()
+    operations_list = utils.init_operations(operations_json)
+    operations_list.sort()
+    utils.print_operations(operations_list)
 
 
 if __name__ == "__main__":
