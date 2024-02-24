d1f042d10a4ceed0282d47bdcbe89fdcd9c13a86
Roman Kudryashov <romkud55@gmail.com>
2024-02-24 13:44:09 +0300

    ADD: Add init_operations func to main.py

--- account_transactions/main.py
+++ account_transactions/main.py
@@ -4,7 +4,8 @@ import utils
 def main():
     filename = "operations.json"
 
-    operations = utils.load_operations(filename)
+    operations_json = utils.load_operations(filename)
+    operations_list = utils.init_operations(operations_json)
 
 
 if __name__ == "__main__":
